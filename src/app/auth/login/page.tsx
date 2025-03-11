ca58a39b28de4f81ac2c9620beb3cf357509255f
RyotaMitaraiWeb <titanium5624@gmail.com>
2025-03-11 13:16:01 +0200

    fix images

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default async function Page() {
 return <section className="p-8 flex flex-col gap-4 lg:gap-16 justify-center items-center lg:flex-row">
-    <Image priority className="max-w-full" src="/assets/register.jpg" alt="" width={612} height={612} />
+    <Image priority className="max-w-full w-[612px] h-auto" width={1000} height={667} src="/assets/login.jpg" alt="" />
     <div className="flex flex-col gap-12 items-center justify-center">
       <Heading className="w-max" level={1}>Влез в твоя профил</Heading>
       <AuthForm action={login} submitButtonText="Влез в профил" successText="Успешно влизане! Сега ще бъдете пренасочени" />
--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 export default async function Page() {
   return <section className="p-8 flex flex-col gap-4 lg:gap-16 justify-center items-center lg:flex-row">
-    <Image priority className="max-w-full" src="/assets/register.jpg" alt="" width={612} height={612} />
+    <Image priority className="max-w-full" src="/assets/register.jpg" alt="" width={612} height={556} />
     <div className="flex flex-col gap-12 items-center justify-center">
       <Heading className="w-max" level={1}>Създай нов профил</Heading>
       <AuthForm action={register} submitButtonText="Създай профил" successText="Успешна регистрация! Сега ще бъдете пренасочени" />
